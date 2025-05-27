14ecd363e66944f5d396910cc923c418d8fa2694
himanshmunjal <munjalhimansh2211@gmail.com>
2025-05-27 19:06:52 +0530

    Go generics

---
+++ GoLang/generics.go
@@ -0,0 +1,18 @@
+package main
+
+// import (
+// 	"fmt"
+// )
+
+// func print_val[G any](items []G) {
+// 	for _, k := range items {
+// 		fmt.Println(k)
+// 	}
+// }
+
+// func main() {
+// 	val := []int{1, 2, 3, 4, 5, 6, 7}
+// 	name := []string{"Himansh", "Rahul", "SODHDIWUE", "JDJHCKD", "KHBCWEDX"}
+// 	print_val(val)
+// 	print_val(name)
+// }
