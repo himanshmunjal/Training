64badb0f0fb6ae3a05d128a5ee4e9a1cd47b887c
himanshmunjal <munjalhimansh2211@gmail.com>
2025-06-11 20:32:32 +0530

    Functions for controlling different functions

---
+++ Backend/controllers/baggage_controller.go
@@ -0,0 +1,19 @@
+package controllers
+
+import (
+
+"github.com/gin-gonic/gin"
+// "github.com/himanshmunjal/Training/models"
+)
+// "net/http"
+
+// "github.com/himanshmunjal/Training/config"
+
+// "github.com/gin-gonic/gin"
+
+func info(c *gin.Context){
+	// var bag models.Baggage
+
+}
+
+// /aci-v1/flightDetails/6E/{flightNumber}/{operationDate}/{departureAirportCode}/{arrivalAirportCode}
